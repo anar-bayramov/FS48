134da093577a6f68c2aca442516a7536517eef3c
anar-bayramov <bayramof.official@gmail.com>
2026-08-29 19:13:22 +0400

    .

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,19 @@
 console.log("My name is Anar");
+
 let name = "Anar";
 console.log(name);
+
 // 1,2,4 xaric digerleri istifade edile biler
+
 let bookPrice = 12;
 let bookAmount = 30;
 let totalPrice = bookPrice * bookAmount;
 console.log(totalPrice);
+
 let kmhSpeed = 90;
 let msSpeed = 6.5 / kmhSpeed;
 console.log(msSpeed);
+
 let day = prompt("Zəhmət olmasa doğum gününüzü qeyd edin");
 let month = prompt("Zəhmət olmasa doğum ayınızı qeyd edin");
 let yearh = prompt("Zəhmət olmasa doğum ilinizi qeyd edin");
@@ -19,12 +24,16 @@ let age = now.getFullYear() - hbd.getFullYear();
 console.log(`Sizin ${age} yaşınız var`);
 let only = age * 365;
 console.log(`Siz ${only} gündür yaşayırsınız`);
+
 let num = 17
 let arti = num * 5;
 let qaliq = num % 3
 console.log(`Dərəcə qüvvəti : ${ arti }`);
 console.log(`Bölgüdən qalan qalıq : ${qaliq}`);
+
 let firstName = "Anar";
 let lastName = "Bayramov";
 console.log(`Sizin adınız ${firstName} ${lastName}. Tanışlığımıza çox şadam!`);
+
 console.log(`Sizi bir daha görməyimizə şadıq, ${[firstName]} ${lastName}`);
+
