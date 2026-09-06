b123ccf35ef15f8f7dbec5e672b9b92e0d7267b9
anar-bayramov <bayramof.official@gmail.com>
2026-09-06 15:55:35 +0400

    Add task 1-5

---
+++ JavaScript/Task 02/main.js
@@ -0,0 +1,51 @@
+// Task 01
+// let number = +"15"; //prompt("Zehmet olmasa 2 reqemli eded daxil edin :");
+
+// while (number < 100) {
+//     number += 7
+//     console.log(number);
+    
+// }
+
+//Task 02
+// let num = +"12"; //prompt("Yazini gormek istediyiniz sayi daxil edin :")
+// let i = 1;
+
+// while (i <= num) {
+//     console.log("I know how to use cycles");
+//     i++;
+// }
+
+//Task 03
+// let number = 100
+
+// while (number < 1000) {
+//     console.log(number);
+//     number+=10
+// }
+
+// Task 04
+// let a = 10
+// let total = 0
+// while (a < 100) {
+//     if (a % 2) {
+//         total += a;
+//     }
+//     a++
+// }
+// console.log(total);
+
+// Task 05
+// let num = +"630" //prompt("3 reqemli istenilen eded daxil edin :");
+// let total = 0;
+// let i = 100
+// while (i < 1000) {
+//     if (!(i % num)) {
+//         total += i;
+//     }
+//     i++
+// }
+
+// console.log(total);
+
+,
