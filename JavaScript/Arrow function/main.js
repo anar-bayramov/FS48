267f255b6de5b2185034dc75e753fac05d6bc61c
anar-bayramov <bayramof.official@gmail.com>
2026-09-22 21:51:09 +0400

    arrow function

---
+++ JavaScript/Arrow function/main.js
@@ -0,0 +1,29 @@
+//Task 01
+// const celsiusToFahrenheit = celsius => (celsius * 1.8) + 32
+// console.log(celsiusToFahrenheit(25));
+
+// Task 02
+// const calculateFuelConsumption = (distance, liters) => {
+//     return (liters / distance) * 100
+// }
+// console.log(calculateFuelConsumption(500,40));
+
+// Task 03
+// const getTotalWithDelivery = orderAmount => {
+//     return orderAmount <= 50 ? "Çatdırılma xidməti : 0 Azn" : "Çatdırılma xidməti : 5 Azn";
+// }
+// console.log(getTotalWithDelivery(55));
+
+// Task 04
+// const getGrade = (score) => {
+//   return score >= 0 && score <= 100
+//     ? score >= 90
+//       ? "Əla"
+//       : score >= 70
+//         ? "Yaxşı"
+//         : score >= 50
+//           ? "Kafi"
+//           : "Kəsildiniz"
+//     : "Düzgün xal daxil edin !";
+// };
+// console.log(getGrade(65));
